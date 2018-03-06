e22b4c360eb0c22be998137c7f769500f3cc25f4
Abhijit Herekar <in.abhijit@gmail.com>
2018-03-05 20:59:28 -0800

    Edit the goroutine files

--- goroutine/goroutine.go
+++ goroutine/goroutine.go
@@ -5,6 +5,7 @@ import "fmt"
 func test(msg chan string, done chan<- bool) {
 	fmt.Println("*****HI ABHI *****\n")
 	temp := <-msg
+	msg <- "Hello from a goroutine!"
 	fmt.Println(temp)
 	done <- true
 	//msg <- "Hello from a goroutine!" //send a string through the channel
@@ -19,9 +20,9 @@ func main() {
 
 	go test(msg, done)
 	<-done
-	//fmt.Println("Main routine waiting for message")
+	fmt.Println("Main routine waiting for message")
 
-	//fmt.Println(<-msg) //receive the string from the channel
+	fmt.Println(<-msg) //receive the string from the channel
 	//fmt.Println(<-msg)
 	//fmt.Println(message)
 
