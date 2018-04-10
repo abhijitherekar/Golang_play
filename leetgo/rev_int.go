38a10ee3c321c577528959111e2efe79e16005f5
Abhijit Herekar <in.abhijit@gmail.com>
2018-04-09 22:20:06 -0700

    Add the overflow check for rev-int

--- leetgo/rev_int.go
+++ leetgo/rev_int.go
@@ -10,6 +10,7 @@ import (
 )
 
 func reverse(num int) int {
+	maxint := 0x7fffffff
 	x := math.Abs(float64(num))
 	result := 0
 	strlen := len(strconv.Itoa(int(x))) - 1
@@ -17,6 +18,9 @@ func reverse(num int) int {
 		result += (int(x) % 10 * int(math.Pow(10, float64(strlen))))
 		x = x / 10
 		strlen -= 1
+	}
+	if result > maxint {
+		return 0
 	}
 	if num < 0 {
 		return result - 2*result
